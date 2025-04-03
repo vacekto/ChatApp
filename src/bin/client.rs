e2759b64e80cbe1a4a44b438203e2775f1911d47
Magic Turtle <vacekto@gmail.com>
2025-04-03 17:50:19 +0200

    networking - server braodcasts messages to other clients, client listens to messages form other clients

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,9 @@
 use std::{
     env,
     error::Error,
-    io::{stdin, Write},
+    io::{stdin, Read, Write},
     net::TcpStream,
+    sync::mpsc::{self, Sender},
     thread,
     time::Duration,
 };
@@ -24,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let addr = (hostname + ":") + &port;
 
-    let mut stream = loop {
+    let read_stream = loop {
         println!("attempting to establish connection..");
         match TcpStream::connect(&addr) {
             Ok(s) => break s,
@@ -37,31 +38,73 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     println!("connection established with: {}", addr);
 
+    let (read_tx, rx) = mpsc::channel();
+
+    let write_tx = read_tx.clone();
+
+    let write_stream = read_stream.try_clone().unwrap();
+
+    let read_handle = thread::spawn(|| handle_read(read_stream, read_tx));
+
+    let write_handle = thread::spawn(move || handle_write(write_stream, write_tx));
+
+    rx.recv().unwrap();
+
+    write_handle.join().unwrap();
+    read_handle.join().unwrap();
+
+    println!("finished");
+
+    Ok(())
+}
+
+fn handle_write(mut stream: TcpStream, tx: Sender<&str>) {
     let mut write_buff = String::new();
     let reader = stdin();
 
     while let Ok(_) = reader.read_line(&mut write_buff) {
         if write_buff == ".quit" {
+            tx.send("quit").unwrap();
             break;
         }
 
         match stream.write(write_buff.as_bytes()) {
             Ok(0) => {
                 println!("server dropped");
+                tx.send("quit").unwrap();
             }
-            Ok(n) => {
-                println!("{}", n);
-            }
+            Ok(_) => {}
             Err(err) => {
                 println!("server error occured: {:?}", err);
+                tx.send("quit").unwrap();
                 break;
             }
         };
         stream.flush().unwrap();
         write_buff.clear();
     }
+}
 
-    println!("finished");
+fn handle_read(mut stream: TcpStream, tx: Sender<&str>) {
+    let mut buff: [u8; 1024] = [0; 1024];
 
-    Ok(())
+    loop {
+        match stream.read(&mut buff) {
+            Ok(0) => {
+                println!("server dropped");
+                tx.send("quit").unwrap();
+                return;
+            }
+            Ok(n) => {
+                let msg = String::from_utf8_lossy(&buff[0..n]);
+                println!("{}", msg);
+            }
+
+            Err(err) => {
+                println!("An error occured: {}. terminating the connection", err);
+                tx.send("quit").unwrap();
+                return;
+            }
+        }
+    }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,16 +1,33 @@
 use std::{
+    collections::HashMap,
     env,
-    io::Read,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
     sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
+        mpsc::{self, Sender},
+        Arc, Mutex,
     },
     thread,
 };
+use uuid::Uuid;
 
 const DEFUALT_HOSTNAME: &str = "localhost";
 const DEFUALT_PORT: &str = "11111";
+
+struct Client {
+    id: Uuid,
+    stream: TcpStream,
+}
+
+struct Message {
+    from: Uuid,
+    text: String,
+}
+
+enum Action {
+    SendMessage(Message),
+    TerminateConnection(Uuid),
+}
 
 fn main() {
     let mut args = env::args();
@@ -26,57 +43,94 @@ fn main() {
     };
 
     let addr = (hostname + ":") + &port;
-
     let listener = TcpListener::bind(addr.to_string()).unwrap();
-
-    let server_up_flag = Arc::new(AtomicBool::new(true));
-
     println!("listening on: {}", addr);
+
+    let clients = Arc::new(Mutex::new(HashMap::<Uuid, Client>::new()));
+
+    let (tx, rx) = mpsc::channel();
+    let also_clients = clients.clone();
+    thread::spawn(move || {
+        while let Ok(a) = rx.recv() {
+            match a {
+                Action::SendMessage(msg) => {
+                    let mut guard = also_clients.lock().unwrap();
+                    for (_, client) in guard.iter_mut() {
+                        if msg.from == client.id {
+                            continue;
+                        }
+                        let stream = &mut client.stream;
+                        stream.write_all(msg.text.as_bytes()).unwrap();
+                    }
+                }
+                Action::TerminateConnection(id) => {
+                    let mut guard = also_clients.lock().unwrap();
+                    guard.remove(&id);
+                }
+            }
+        }
+    });
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                // let server_up_flag = server_up_flag.clone();
-                thread::spawn(|| handle_connection(stream));
+                let client = Client {
+                    id: Uuid::new_v4(),
+                    stream,
+                };
+
+                let client_cloned = Client {
+                    id: client.id,
+                    stream: client.stream.try_clone().unwrap(),
+                };
+                let tx = tx.clone();
+                let mut clients_guard = clients.lock().unwrap();
+                clients_guard.insert(client.id, client_cloned);
+                // clients_guard.insert(client.id, client_clone);
+
+                thread::spawn(|| handle_connection(client, tx));
             }
             Err(e) => println!("Connection failed: {}", e),
         }
     }
-
-    server_up_flag.store(false, Ordering::Relaxed);
 }
 
-fn handle_connection(
-    mut stream: TcpStream,
-    // server_up_flag: Arc<AtomicBool>,
-) -> Result<(), std::io::Error> {
-    println!("connection established: {}", stream.peer_addr()?);
-    let mut buff = [0; 1024];
+fn handle_connection(mut client: Client, public_room: Sender<Action>) {
+    println!(
+        "connection established: {}",
+        client.stream.peer_addr().unwrap()
+    );
+    let mut buff: [u8; 1024] = [0; 1024];
 
-    // stream.set_nonblocking(true).unwrap();
+    // client.stream.set_nonblocking(true).unwrap();
 
     loop {
-        match stream.read(&mut buff) {
+        match client.stream.read(&mut buff) {
             Ok(0) => {
-                println!("Client {:?} disconnect", stream.peer_addr());
+                println!("Client {:?} disconnect", client.stream.peer_addr());
+                public_room
+                    .send(Action::TerminateConnection(client.id))
+                    .unwrap();
                 break;
             }
             Ok(n) => {
-                let received_str = String::from_utf8_lossy(&buff[0..n]);
-                println!("New message:  {}", received_str);
+                let msg = String::from_utf8_lossy(&buff[0..n]);
+                let msg = Message {
+                    text: msg.to_string(),
+                    from: client.id,
+                };
+                public_room.send(Action::SendMessage(msg)).unwrap();
             }
             // Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
-            //     if !server_up_flag.load(Ordering::Relaxed) {
-            //         break;
-            //     }
             //     thread::sleep(Duration::from_millis(100));
-            //     println!("cosikdosi");
             // }
             Err(err) => {
                 println!("An error occured: {}. terminating the connection", err);
-                return Err(err);
+                public_room
+                    .send(Action::TerminateConnection(client.id))
+                    .unwrap();
+                return;
             }
-        };
+        }
     }
-    Ok(())
 }
